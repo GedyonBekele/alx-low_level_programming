9c7d01fc38c58bc83a6240073b0a0ba4ded5bfc5
GedyonBekele <gedyonbekele@gmail.com>
2023-06-20 03:44:39 -0700

    try

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,30 +1,20 @@
-/* 
-This program defines a new type struct dog with the following elements: name, age, and owner.
-*/
-
 #include <stdio.h>
-#include <stdlib.h>
 
-// Define the struct dog
-struct dog {
+/* Define the struct dog */
+typedef struct dog {
 char *name;
 float age;
 char *owner;
-};
+} dog;
 
-int main() {
-// Create a new dog
-struct dog my_dog;
-    
-// Assign values to the dog's elements
-my_dog.name = "Fido";
-my_dog.age = 2.5;
-my_dog.owner = "John";
-    
-// Print out the dog's information
+int main(void) {
+/* Create a new dog */
+dog my_dog = {"Fido", 3.5, "John"};
+
+/* Print the dog's information */
 printf("Name: %s\n", my_dog.name);
 printf("Age: %.1f\n", my_dog.age);
 printf("Owner: %s\n", my_dog.owner);
-    
+
 return 0;
 }
